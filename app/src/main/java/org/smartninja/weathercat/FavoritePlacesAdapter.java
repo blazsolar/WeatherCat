0f5ef6d948d4324f97ccccc6c8de72431a92b5b0
Blaz Solar <solarblaz@gmail.com>
2016-05-23 17:03:54 +0200

    Rename languages

--- app/src/main/java/org/smartninja/weathercat/FavoritePlacesAdapter.java
+++ app/src/main/java/org/smartninja/weathercat/FavoritePlacesAdapter.java
@@ -9,8 +9,6 @@ import android.widget.TextView;
 
 import org.smartninja.weathercat.model.WeatherData;
 
-import java.util.List;
-
 /**
  * Created by blaz on 08/05/16.
  */
@@ -18,23 +16,23 @@ public class FavoritePlacesAdapter extends BaseAdapter {
 
     public static final WeatherData[] DATA = new WeatherData[] {
             new WeatherData(3239318, "Mestna Občina Ljubljana", 13.91f),
-            new WeatherData(3239318, "Mestna Občina Ljubljana", 13.91f),
-            new WeatherData(3239318, "Mestna Občina Ljubljana", 13.91f),
-            new WeatherData(3239318, "Mestna Občina Ljubljana", 13.91f),
-            new WeatherData(3239318, "Mestna Občina Ljubljana", 13.91f),
-            new WeatherData(3239318, "Mestna Občina Ljubljana", 13.91f),
-            new WeatherData(3239318, "Mestna Občina Ljubljana", 13.91f),
-            new WeatherData(3239318, "Mestna Občina Ljubljana", 13.91f),
-            new WeatherData(3239318, "Mestna Občina Ljubljana", 13.91f),
-            new WeatherData(3239318, "Mestna Občina Ljubljana", 13.91f),
-            new WeatherData(3239318, "Mestna Občina Ljubljana", 13.91f),
-            new WeatherData(3239318, "Mestna Občina Ljubljana", 13.91f),
-            new WeatherData(3239318, "Mestna Občina Ljubljana", 13.91f),
-            new WeatherData(3239318, "Mestna Občina Ljubljana", 13.91f),
-            new WeatherData(3239318, "Mestna Občina Ljubljana", 13.91f),
-            new WeatherData(3239318, "Mestna Občina Ljubljana", 13.91f),
-            new WeatherData(3239318, "Mestna Občina Ljubljana", 13.91f),
-            new WeatherData(3239318, "Mestna Občina Ljubljana", 13.91f)
+            new WeatherData(3186843, "Občina Žalec", 13.91f),
+            new WeatherData(3192062, "Občina Radovljica", 13.91f),
+            new WeatherData(3197378, "Kranj", 13.91f),
+            new WeatherData(3194351, "Novo Mesto", 13.91f),
+            new WeatherData(3198647, "Jesenice", 13.91f),
+            new WeatherData(3192241, "Ptuj", 13.91f),
+            new WeatherData(3195506, "Maribor", 13.91f),
+            new WeatherData(5128638, "New York", 13.91f),
+            new WeatherData(1689973, "San Francisco", 13.91f),
+            new WeatherData(3186886, "Zagreb", 13.91f),
+            new WeatherData(2759794, "Amsterdam", 13.91f),
+            new WeatherData(5056033, "London", 13.91f),
+            new WeatherData(2950159, "Berlin", 13.91f),
+            new WeatherData(2988507, "Paris", 13.91f),
+            new WeatherData(292223, "Dubai", 13.91f),
+            new WeatherData(1609350, "Bangkok", 13.91f),
+            new WeatherData(1138958, "Kabul", 13.91f)
     };
 
     private final LayoutInflater layoutInflater;
@@ -47,7 +45,7 @@ public class FavoritePlacesAdapter extends BaseAdapter {
 
     @Override
     public int getCount() {
-        if (data != null) {
+        if (data != null) {git 
             return data.length;
         } else {
             return 0;
